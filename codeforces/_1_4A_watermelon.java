628b9c32c91f1a7503bf064071b179cecfc1835a
raghavagarwal <raghavagarwal3618@gmail.com>
2025-10-21 18:58:56 +0530

    day 1, 2 ques

---
+++ codeforces/_1_4A_watermelon.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class _1_4A_watermelon{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num % 2 == 0 && num > 2){
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
---
+++ codeforces/_2_71A_longWords.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class _2_71A_longWords {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (n-- > 0) {
+            String word = br.readLine();
+            int len = word.length();
+            if (len > 10) sb.append(word.charAt(0)).append(len - 2).append(word.charAt(len - 1)).append('\n');
+            else sb.append(word).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
